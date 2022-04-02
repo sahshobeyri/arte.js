c0780045d826c23117570a5712b7d83ef37f1ec1
sahshobeyri <sahshobeyri@gmail.com>
2022-04-02 06:05:00 +0430

    add drawGrid

--- first-test/index.js
+++ first-test/index.js
@@ -7,7 +7,21 @@ function cls() {
   ctx.clearRect(0, 0, cw, ch);
 }
 
-const r = 1/2
+function drawGrid(x,y,w,h,xFreq,yFreq) {
+  const xStep = w/xFreq
+  for (let i = 0; i < xFreq; i++) {
+    ctx.moveTo(x+(i*xStep),y)
+    ctx.lineTo(x+(i*xStep),y+h)
+  }
+
+  const yStep = h/yFreq
+  for (let i = 0; i < yFreq; i++) {
+    ctx.moveTo(x,y+(i*yStep))
+    ctx.lineTo(x+w,y+(i*yStep))
+  }
+}
+
+const r = 0.5
 const outerBox = {
   x:0,
   y:0,
@@ -35,6 +49,8 @@ for (let i = 0; i < freq; i++) {
   ctx.moveTo(outerBox.x,outerBox.y + outerBox.h* (i/freq));
   ctx.lineTo(innerBox.x,innerBox.y + innerBox.h* (i/freq));
 }
+
+drawGrid(innerBox.x,innerBox.y,innerBox.w,innerBox.h,10,10)
 
 ctx.stroke();
 
